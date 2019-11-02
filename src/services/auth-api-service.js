0621bbaf391305fe8a500771ebce6a12d123909b
Leon Dail <leondailofficial@gmail.com>
2019-11-02 14:52:20 -0700

    added functional registration

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -18,6 +18,24 @@ const AuthApiService = {
         return res.json();
       }
     })
+  },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => {
+      if (!res.ok) {
+        return res.json()
+          .then(e => Promise.reject(e))
+      }
+      else {
+        return res.json();
+      }
+    })
   }
 }
 
